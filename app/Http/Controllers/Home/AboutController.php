f55754166aa06606a5a70ac615849079450dba32
Tasneem <tasneemm967@gmail.com>
2025-09-18 18:14:29 +0200

    update multi_images in about_page backend

--- app/Http/Controllers/Home/AboutController.php
+++ app/Http/Controllers/Home/AboutController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use App\Models\About;
+use App\Models\MultiImage;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
 
@@ -80,14 +82,119 @@ class AboutController extends Controller
         return view('frontend/about_page', compact('aboutPage'));
         
     }
+
+    public function AboutMultiImage() {
+
+        return view('admin/about_page/multimage');
+
+    }
     
 
+    public function StoreMultiImage(Request $request) {
+
+        $image = $request->file('multi_image');
+
+        foreach ($image as $multi_image) {
+
+            $name_gen = hexdec(uniqid()). '.' .$multi_image->getClientOriginalExtension();
+
+            Image::make($multi_image)->resize(220, 220)->save(public_path('upload/multi/'. $name_gen));
+
+            $save_url = $name_gen;
+
+
+            MultiImage::insert([
+
+                'multi_image' => $save_url,
+                'created_at' => Carbon::now()
+            ]);
+
+        } //end foreach
+
+            $notification = array(
+
+                'message' => 'Multi Images Inserted Successfully',
+                'alert-type' => 'success'
+            );
+
+            return redirect()->route('all.multi.image')->with($notification);
+
+
+    }
+
+    public function AllMultiImage() {
+
+        $allMultiImage = MultiImage::all();
+
+        return view('admin.about_page.all_multiimage', compact('allMultiImage'));
+
+    }
+
+    public function EditMultiImage($id) {
+
+        $multiImage = MultiImage::findOrFail($id);
+        return view('admin.about_page.edit_multi_image', compact('multiImage'));
+
+    }
+
+    public function UpdateMultiImage(Request $request) {
+
+        $multi_image_id = $request->id;
+
+        if ($request->file('multi_image')) {
+
+            $image = $request->file('multi_image');
+            $name_gen = hexdec(uniqid()). '.' .$image->getClientOriginalExtension();
+
+            Image::make($image)->resize(220, 220)->save(public_path('upload/multi/'. $name_gen));
+
+            $save_url = $name_gen;
+
+
+            MultiImage::findOrFail($multi_image_id)->update([
+
+                'multi_image' => $save_url
+            ]);
+
+      
+        $notification = array(
+
+            'message' => 'Image Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.multi.image')->with($notification);
+
+        }
+
+    }
+
+    public function DeleteMultiImage($id) {
+
+        $multi = MultiImage::findOrFail($id);
+        $img = $multi->multi_image;
+        unlink('upload/multi/'.$img);
+
+        MultiImage::findOrFail($id)->delete();
+
+        $notification = array(
+
+            'message' => 'Image Deleted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+
+    }
 
 
 
-
-
-
-
-    
 }
+
+
+
+
+
+
+    
+
